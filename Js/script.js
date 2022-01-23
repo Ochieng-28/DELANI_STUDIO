a7932b9d120c780bd4b08af9740f0f3604fea98a
Tolaru <toniolaru.gmail.com>
2022-01-23 13:45:02 +0300

    created the Jquerry script for toggling on the portfolio to desplay hiden text.

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+// alert("workingggg")
 $(document).ready(function() {
     $("#design").click(function(){
         // action on mouse toggles over design
@@ -18,6 +19,27 @@ $(document).ready(function() {
       })
     })
   })
+  $(document).ready(function() {
+    $("#develop").click(function(){
+        // action on mouse toggles over design
+      $(".secondClick").toggle();
+      $("#develop-show").toggle();
+      $("#develop-show").css("textAlign","center");
+      $("p,h6")
+      .on("mouseenter",function() {
+        $(this).css({
+          "font-weight":"bolder"
+        });
+      }) 
+    //    action on mouseleave to restore to default
+      .on("mouseleave", function(){
+        var styles={
+          fontWeight:"normal"
+        }
+      })
+    })
+  })
+
 
   $(document).ready(function() {
       //    action on mouseenter on the product management
@@ -40,12 +62,23 @@ $(document).ready(function() {
     })
   })
 
-  $(document).ready(function(){
+//   $(document).ready(function(){
 
-    $("Id4work4").mouseover(function(){
-      $(".overlay4").show({opacity: '1'});
-    }).mouseout(function(){
-      $(".overlay4").hide({opacity: '0'});
-    });
+//     $("Id4work4").mouseover(function(){
+//       $(".overlay4").show({opacity: '1'});
+//     }).mouseout(function(){
+//       $(".overlay4").hide({opacity: '0'});
+//     });
   
-    });
+//     });
+
+$(document).ready(function() {
+  $("#blanks").submit(function(event) {
+      var nameInput = $("input#name").val();
+      var emailInput = $("input#email").val();
+      var messageInput = $("input#message").val();
+      $(".name").text(nameInput);
+      $(".email").text(emailInput);
+      $(".message").text(messageInput);
+   });
+  });
